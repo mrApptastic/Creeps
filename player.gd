1671348f056163935265efe692acc77eb67df060
mrApptastic <henrikbeske@gmail.com>
2026-09-12 15:39:17 +0200

    More creeps n stuff

--- player.gd
+++ player.gd
@@ -8,7 +8,7 @@ var screen_size # Size of the game window.
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	screen_size = get_viewport_rect().size
-	#hide()
+	hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -49,3 +49,6 @@ func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
+	
+	
+	
